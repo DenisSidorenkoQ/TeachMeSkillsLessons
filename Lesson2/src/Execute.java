991cab29a020998dc0c0b2d90268780c525d8aaa
Denis <apricot103@mail.ru>
2022-07-06 01:00:50 +0300

    MusicPlayer realisation

--- Lesson2/src/Execute.java
+++ Lesson2/src/Execute.java
@@ -5,6 +5,7 @@ public class Execute
         MusicPlayer mp = new MusicPlayer();
         mp.change_music("Name", "Name", "3:21");
         mp.change_music("Name", "Name", "3:21");
+        mp.volume_down(30);
         mp.output();
     }
 }
