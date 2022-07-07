6fcdf146d9dab31785de85e1accbd7a5862c4624
Denis <apricot103@mail.ru>
2022-07-08 00:07:18 +0300

    Lesson 3 realisation

---
+++ Lesson3/src/Main.java
@@ -0,0 +1,33 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Task1.outputMonthByNumber(8);
+        Task1.outputNameOfTheSeasonByMonthNumber(8);
+        Task1.evenChecking(5);
+        Task1.checkTemperature(-7);
+        Task1.checkColor(3);
+        System.out.println("\n\n\n");
+
+        Task2.outputOddNumbers();
+        System.out.println("\n");
+        Task2.outputNumbersFromFiveToOne();
+        System.out.println("\n");
+        Task2.sumNumber();
+        System.out.println("\n");
+        Task2.outputNumbers();
+        System.out.println("\n");
+        Task2.outputTenNumbers();
+        System.out.println("\n");
+        Task2.numberSquares();
+        System.out.println("\n\n\n");
+
+        Task3.Fibonacci();
+        System.out.println("\n");
+        Task3.bankOperation();
+        System.out.println("\n");
+        Task3.multiplicationTable();
+    }
+}
+
+
