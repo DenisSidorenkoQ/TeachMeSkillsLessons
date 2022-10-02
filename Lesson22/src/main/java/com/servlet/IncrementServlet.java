5f5050c44745a8005f041e28cb2676dd2389abe5
Denis <apricot103@mail.ru>
2022-10-02 16:10:46 +0300

    Class rename, override init method, change doGet method and add access modifiers

--- Lesson22/src/main/java/com/servlet/IncrementServlet.java
+++ Lesson22/src/main/java/com/servlet/IncrementServlet.java
@@ -9,15 +9,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static java.lang.System.out;
-@WebServlet("/test")
-public class HelloServlet extends HttpServlet {
-    AtomicInteger counter = new AtomicInteger(0);
+@WebServlet("/increment")
+public class IncrementServlet extends HttpServlet {
+    private final int DEFAULT_COUNTER_VALUE = 0;
+    private AtomicInteger counter;
+
+    @Override
+    public void init() throws ServletException {
+        counter = new AtomicInteger(DEFAULT_COUNTER_VALUE);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         try (PrintWriter writer = resp.getWriter()){
-            writer.write("<h2>Method: " + req.getMethod() + "</h2><h2>Counter: " + counter + "</h2>IP: " + req.getLocalAddr());
+            writer.write("<h2>Method: " + req.getMethod() + "</h2><h2>Counter: " + counter + "</h2><h2>Servlet name: " +
+                    req.getHttpServletMapping().getServletName() + "</h2>IP: " + req.getLocalAddr());
         }
     }
 
