6361c49e8e2aac2328d4d9bc099d7426aca31e12
Denis <apricot103@mail.ru>
2022-10-23 22:48:07 +0300

    changed tests

--- JavaEE/TestsFromLesson27/src/test/java/TwoSumTest.java
+++ JavaEE/TestsFromLesson27/src/test/java/TwoSumTest.java
@@ -1,21 +1,36 @@
 import org.example.TwoSums;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class TwoSumTest {
     private final TwoSums twoSums = new TwoSums();
 
-    @ParameterizedTest
-    @CsvSource({
-            "30",
-            "40",
-            "50",
-            "60"
-    })
-    public void shouldConvertCorrectly(int desiredResult) {
-        int[] answer = twoSums.calculate(new int[]{10, 20, 30, 40, 50, 60}, desiredResult);
-
-        Assertions.assertNotNull(answer);
+    @Test
+    public void testCalculatedMethodIfArrayNotNull() {
+        TwoSums twoSums = new TwoSums();
+        int[] array = {10, 20, 30, 40, 50, 60};
+        int desiredResult = 30;
+        int[] expectedArray = {0, 1};
+        int[] answer = twoSums.calculate(array, desiredResult);
+        Assertions.assertArrayEquals(expectedArray, answer);
     }
+
+    @Test
+    public void testCalculatedMethodIfArrayNull() {
+        TwoSums twoSums = new TwoSums();
+        int[] array = null;
+        int desiredResult = 30;
+        Assertions.assertNull(twoSums.calculate(array, desiredResult));
+    }
+
+    @Test
+    public void testCalculatedMethodIfNoSolution() {
+        TwoSums twoSums = new TwoSums();
+        int[] array = {30, 40, 50, 60};
+        int desiredResult = 30;
+        Assertions.assertNull(twoSums.calculate(array, desiredResult));
+    }
+
 }
