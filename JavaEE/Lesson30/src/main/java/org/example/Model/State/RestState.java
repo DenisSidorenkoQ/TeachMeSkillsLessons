4dd7b9b01bcacec1d38b1e5ef70ef81c8ead1e70
Denis <apricot103@mail.ru>
2022-11-06 20:58:11 +0300

    fix

--- JavaEE/Lesson30/src/main/java/org/example/Model/State/RestState.java
+++ JavaEE/Lesson30/src/main/java/org/example/Model/State/RestState.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class RestState implements OrganizationMemberState {
     @Override
     public boolean executeTask(OrganizationMember organizationMember, String task) {
+        System.out.println("Я отдыхаю, задача не сделана");
         return false;
     }
 }
