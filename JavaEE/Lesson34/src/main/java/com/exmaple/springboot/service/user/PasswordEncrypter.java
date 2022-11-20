663f6e32ac696d8f177fdce0c316206db2570233
Denis <apricot103@mail.ru>
2022-11-20 20:25:44 +0300

    fix

--- JavaEE/Lesson34/src/main/java/com/exmaple/springboot/service/user/PasswordEncrypter.java
+++ JavaEE/Lesson34/src/main/java/com/exmaple/springboot/service/user/PasswordEncrypter.java
@@ -1,16 +1,21 @@
 package com.exmaple.springboot.service.user;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
 
 @Component
 public class PasswordEncrypter {
+    @Autowired
+    private Environment env;
     private final BCrypt.Hasher hasher;
 
     public PasswordEncrypter() {
-        hasher = BCrypt.with(new SecureRandom("SALT".getBytes()));
+        String salt = env.getProperty("salt");
+        hasher = BCrypt.with(new SecureRandom(salt.getBytes()));
     }
 
     public String getEncryptedPassword(String password) {
