69c4dce3b320d8f2731f1046f69f0f907736c2b6
Denis <apricot103@mail.ru>
2022-11-26 13:38:21 +0300

    fix

--- JavaEE/Lesson35-36/src/main/java/com/exmaple/springboot/service/user/UserService.java
+++ JavaEE/Lesson35-36/src/main/java/com/exmaple/springboot/service/user/UserService.java
@@ -6,7 +6,6 @@ import com.exmaple.springboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
