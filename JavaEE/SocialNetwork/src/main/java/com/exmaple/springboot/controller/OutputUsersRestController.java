dcf66f62198a754dfe0a2e262bd6ee5b48e8b64d
Denis <apricot103@mail.ru>
2022-11-27 14:22:37 +0300

    fix checkstyle

--- JavaEE/SocialNetwork/src/main/java/com/exmaple/springboot/controller/OutputUsersRestController.java
+++ JavaEE/SocialNetwork/src/main/java/com/exmaple/springboot/controller/OutputUsersRestController.java
@@ -5,7 +5,10 @@ import com.exmaple.springboot.dto.UserDto;
 import com.exmaple.springboot.model.User;
 import com.exmaple.springboot.service.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,7 +24,6 @@ public class OutputUsersRestController {
                                         @RequestParam(required = false) Integer pageNumber) {
         List<User> users;
         users = userService.getUserFromPage(pageSize, pageNumber);
-
         userConverter.toDto(users);
         return userConverter.toDto(users);
     }
